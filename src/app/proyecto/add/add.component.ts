c336856a494c616901b067d7b7e63b56dd8b4a89
lorenagiraldo <lorenagiraldo1031@gmail.com>
2017-10-19 14:59:01 -0500

    modificacion3

--- src/app/proyecto/add/add.component.ts
+++ src/app/proyecto/add/add.component.ts
@@ -10,6 +10,25 @@ import {ProyectoDataServerService} from '../proyecto-data-server.service'
 export class AddComponent implements OnInit {
 
   public proyecto: Proyecto;
+  
+  listaTipo = [
+    { value: 'CURSO' },
+    { value: 'SIMPOSIO' },
+    { value: 'SEMINARIO' },
+    { value: 'DIPLOMADO' },
+    { value: 'TALLER'},
+    { value: 'JORNADA'}    
+  ];
+  
+  listaLugar = [
+    { value: 'MANIZALES' },
+    { value: 'MEDELLIN' },
+    { value: 'BOGOTA' },
+    { value: 'PEREIRA' },
+    { value: 'ARMENIA'},
+    { value: 'IBAGUE'}    
+  ];
+  
 
   constructor(private proyectoService:ProyectoDataServerService) { }
 
