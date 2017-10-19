50af9dbe8a240b0ef41d4ab045e0e79496657bff
lorenagiraldo <lorenagiraldo1031@gmail.com>
2017-10-19 15:56:23 -0500

    modificacion7

--- src/app/proyecto/list/list.component.ts
+++ src/app/proyecto/list/list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ProyectoDataServerService } from '../proyecto-data-server.service';
 
 @Component({
   selector: 'app-list',
@@ -7,9 +8,10 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  constructor(private proyectoService:ProyectoDataServerService) { }
 
   ngOnInit() {
+    this.proyectoService.cargarProyectos();
   }
 
 }
