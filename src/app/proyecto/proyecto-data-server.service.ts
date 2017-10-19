9aec463dd67daeb6702d387979170a108e183ee5
lorenagiraldo <lorenagiraldo1031@gmail.com>
2017-10-19 15:22:43 -0500

    modificacion6

--- src/app/proyecto/proyecto-data-server.service.ts
+++ src/app/proyecto/proyecto-data-server.service.ts
@@ -1,8 +1,28 @@
 import { Injectable } from '@angular/core';
+import { Proyecto } from './Proyecto';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable()
 export class ProyectoDataServerService {
+  
+  listaProyectos: Array<Proyecto>;
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+  
+  cargarProyectos()
+  {
+    this.http.get('http://localhost:49475/Proyecto/ListaProyectos').subscribe(data=>
+  {
+    this.listaProyectos= data as Array<Proyecto>;
+  });
+  }
+  
+  guardarProyecto(proyecto:Proyecto)
+  {
+    let cadena:string=`?titulo=${proyecto.titulo}&objetivo=${proyecto.objetivo}&tipo=${proyecto.tipo}&duracion=${proyecto.duracion}&lugar=${proyecto.lugar}`;
+    this.http.get('http://localhost:49475/Proyecto/guardarProyecto'+cadena).subscribe(data =>
+    {
+    });
+  }
 
 }
